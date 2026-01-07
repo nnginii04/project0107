e3d181e19a530162244d462ebf46c7a85d92324a
yejin <nnginii040416@gmail.com>
2026-01-07 11:53:18 +0900

    first commit

---
+++ src/main/java/com/example/project0107/Project0107Application.java
@@ -0,0 +1,13 @@
+package com.example.project0107;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project0107Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project0107Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project0107/Project0107ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project0107;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project0107ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
